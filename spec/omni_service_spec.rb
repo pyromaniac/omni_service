222d3b3e8ba59dd233404bfac8f527f66c9c9bea
Arkadiy Zabazhanov <kinwizard@gmail.com>
2025-11-27 09:45:17 +0700

    Initial commit

---
+++ spec/omni_service_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OmniService do
+  it "has a version number" do
+    expect(OmniService::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
